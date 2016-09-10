9483285f473e32ed7c49145d9ecb68810f7b78e8
Ray Lugo, Jr <rlugojr@gmail.com>
2016-09-10 04:45:46 -0400

    Added Google Places Support
    Added Google Places API
    Changed regular markers for pictures of sites.

--- js/app.js
+++ js/app.js
@@ -4,7 +4,7 @@ var myLocations = [
 		siteName: "Cristo Redentor",
 		lat: -22.9518763,
 		lng: -43.2112374,
-		placeId: "ChIJJ332e9V_mQARSqf2EfNFdWI",
+		placeId: "ChIJP6FKmNV_mQAR3gKVAdeEyZ0",
 		wikiKey: "Christ_the_Redeemer_(statue)",
 		country: "Brazil"
     },
@@ -13,7 +13,7 @@ var myLocations = [
 		siteName: "The Great Wall of Badaling",
 		lat: 40.357584,
 		lng: 116.017822,
-		placeId: "ChIJt1fzi2iT8DUROrvH_aNA5FY",
+		placeId: "ChIJFbdOF22T8DURnEAe0oca7LM",
 		wikiKey: "Great_Wall_of_China",
 		country: "China"
     },
@@ -22,7 +22,7 @@ var myLocations = [
 		siteName: "Mausoleum of the First Qin Emperor",
 		lat: 34.377564,
 		lng: 109.258013,
-		placeId: "ChIJOWqGrZagZDYRM_pzMR8VVsU",
+		placeId: "ChIJCZeK772gZDYR3ERa7J2_JdY",
 		wikiKey: "Mausoleum_of_the_First_Qin_Emperor",
 		country: "China"
     },
@@ -31,7 +31,7 @@ var myLocations = [
 		siteName: "Alcazar de Diego Colon",
 		lat: 18.4773854,
 		lng: -69.8830572,
-		placeId: "ChIJxYO7lhWIr44R1FZ9oQ5d_5I",
+		placeId: "ChIJV3ejOxaIr44R5tZj2EjXHWU",
 		wikiKey: "Alcázar_de_Colón",
 		country: "Dominican Republic"
     },
@@ -40,7 +40,7 @@ var myLocations = [
 		siteName: "The Great Pyramid at Giza",
 		lat: 29.9772962,
 		lng: 31.1303015,
-		placeId: "ChIJYVt6XIdFWBQRflbPmEWFZR8",
+		placeId: "ChIJGymPrIdFWBQRJCSloj8vDIE",
 		wikiKey: "Great_Pyramid_of_Giza",
 		country: "Egypt"
     },
@@ -49,7 +49,7 @@ var myLocations = [
 		siteName: "Great Spinx of Giza",
 		lat: 29.97496,
 		lng: 31.1364519,
-		placeId: "ChIJu2RSrWNPWBQR0cg8tHi8jd4",
+		placeId: "ChIJeamuo2JPWBQRqb1mQKbw08k",
 		wikiKey: "Great_Sphinx_of_Giza",
 		country: "Egypt"
     },
@@ -58,7 +58,7 @@ var myLocations = [
 		siteName: "Mont-Saint-Michel and its Bay",
 		lat: 48.6360166,
 		lng: -1.5133085,
-		placeId: "ChIJFePSVtaoDkgR6tjrKGsr9iY",
+		placeId: "ChIJwccMN0RYBkgRlp_YeSIlOzs",
 		wikiKey: "Mont_Saint-Michel_Bay",
 		country: "France"
     },
@@ -76,7 +76,7 @@ var myLocations = [
 		siteName: "Chateau de Versailles",
 		lat: 48.8073846,
 		lng: 2.1052098,
-		placeId: "ChIJx5lWWLl95kcRq3xG_hvqOwg",
+		placeId: "ChIJdUyx15R95kcRj85ZX8H8OAU",
 		wikiKey: "Palace_of_Versailles",
 		country: "France"
     },
@@ -85,7 +85,7 @@ var myLocations = [
 		siteName: "Wartburg Castle",
 		lat: 50.9663423,
 		lng: 10.3041484,
-		placeId: "ChIJA9aMQXCcpEcRr084TctUNxY",
+		placeId: "ChIJMR-Kc3CcpEcR0OqU8OullQg",
 		wikiKey: "Wartburg",
 		country: "Germany"
     },
@@ -103,7 +103,7 @@ var myLocations = [
 		siteName: "Acropolis, Athens",
 		lat: 37.9715323,
 		lng: 23.7235552,
-		placeId: "ChIJ86z1Nxi9oRQR9g3r9ULAl1w",
+		placeId: "ChIJhaTPiGV5XxMRW7GMCp-Rul8",
 		wikiKey: "Acropolis_of_Athens",
 		country: "Greece"
     },
@@ -112,7 +112,7 @@ var myLocations = [
 		siteName: "Mount Athos",
 		lat: 40.1535416,
 		lng: 24.3155917,
-		placeId: "ChIJNQyybXRIrxQRSDqjtAKQDRc",
+		placeId: "ChIJWTaPYaFIrxQREX9MeLyviB8",
 		wikiKey: "Mount_Athos",
 		country: "Greece"
     },
@@ -121,7 +121,7 @@ var myLocations = [
 		siteName: "Vatican City",
 		lat: 41.902141,
 		lng: 12.453926,
-		placeId: "ChIJJTk-DGZgLxMRPGxQNTiMSQA",
+		placeId: "ChIJPS3UVwqJJRMRsH46sppPCQA",
 		wikiKey: "Vatican_City",
 		country: "Holy See"
     },
@@ -130,7 +130,7 @@ var myLocations = [
 		siteName: "Taj Mahal",
 		lat: 27.1750151,
 		lng: 78.0399612,
-		placeId: "ChIJoa7KUCBxdDkRXDAJ-ll67vU",
+		placeId: "ChIJPRQcHyBxdDkRs1lw_Dj1QnU",
 		wikiKey: "Taj_Mahal",
 		country: "India"
     },
@@ -139,7 +139,7 @@ var myLocations = [
 		siteName: "Persepolis",
 		lat: 29.9355133,
 		lng: 52.8904617,
-		placeId: "ChIJ77o-qV7jrT8RWHhg97btquE",
+		placeId: "ChIJba3wXF_jrT8R7s-Xacc-NgI",
 		wikiKey: "Persepolis",
 		country: "Iran"
     }
@@ -148,7 +148,7 @@ var myLocations = [
 
 // Declaring global variables now to satisfy strict mode
 //var map;
-var $, ko, clientID, clientSecret;
+var $, ko, bounds, placesService;
 
 
 $( document ).on( "click", function ( e ) {
@@ -163,7 +163,6 @@ $( document ).on( "click", function ( e ) {
 
 var Location = function ( data ) {
 	var self = this;
-
 	var wikiVals = [];
 
 
@@ -171,7 +170,7 @@ var Location = function ( data ) {
 	this.siteName = data.siteName;
 	this.lat = data.lat;
 	this.lng = data.lng;
-	this.latLng = this.lat + ', ' + this.lng;
+	this.latLng = ( data.lat + ", " + data.lng );
 	this.placeId = data.placeId;
 	this.wikiKey = data.wikiKey;
 	this.country = data.country;
@@ -195,11 +194,11 @@ var Location = function ( data ) {
 
 			var arrExtract = jsonPath( resp, "$..extract" );
 			self.wikiExtract = arrExtract[ 0 ];
-			console.log( self.wikiExtract );
+			//console.log( self.wikiExtract );
 
 			var arrThumb = jsonPath( resp, "$..thumbnail" );
 			self.wikiThumb = arrThumb[ 0 ].source;
-			console.log( self.wikiThumb );
+			//console.log( self.wikiThumb );
 		} )
 		.fail( function ( jqXHR, textStatus ) {
 			console.log( "error" );
@@ -221,6 +220,50 @@ var Location = function ( data ) {
 
 	var highlightedIcon = makeMarkerIcon( 'ffff24' );
 
+	var Request = {
+		placeId: data.placeId
+	};
+
+	placesService = new google.maps.places.PlacesService( map );
+
+	this.marker = new google.maps.Marker( {} );
+
+	placesService.getDetails( Request, callback );
+
+	function callback( place, status ) {
+		console.log( status );
+
+		if ( status == google.maps.places.PlacesServiceStatus.OK ) {
+			createPhotoMarker( place );
+		}
+	}
+
+	function createPhotoMarker( place ) {
+		var photos = place.photos;
+		console.log( place.toString );
+		if ( !photos ) {
+			self.marker = new google.maps.Marker( {
+				position: new google.maps.LatLng( data.lat, data.lng ),
+				map: map,
+				title: data.siteName,
+				icon: defaultIcon,
+				animation: google.maps.Animation.DROP,
+			} );
+
+		} else {
+			self.marker = new google.maps.Marker( {
+				map: map,
+				position: place.geometry.location,
+				title: place.name,
+				icon: photos[ 0 ].getUrl( {
+					'maxWidth': 75,
+					'maxHeight': 75
+				} ),
+				animation: google.maps.Animation.DROP,
+			} );
+		}
+	}
+
 
 	this.info = '<div class="info-window-content"><div class="title"><b>' + data.siteName + "</b></div>" +
 		'<div class="infoContent"><p>' + this.latLng + '</p></div>';
@@ -229,46 +272,47 @@ var Location = function ( data ) {
 		content: self.info
 	} );
 
-	this.marker = new google.maps.Marker( {
-		position: new google.maps.LatLng( data.lat, data.lng ),
-		map: map,
-		title: data.siteName,
-		icon: defaultIcon,
-		animation: google.maps.Animation.DROP,
-	} );
+	// this.marker = new google.maps.Marker( {
+	// 	position: new google.maps.LatLng( data.lat, data.lng ),
+	// 	map: map,
+	// 	title: data.siteName,
+	// 	icon: defaultIcon,
+	// 	animation: google.maps.Animation.DROP,
+	// } );
 
-	this.showMarker = ko.computed( function () {
-		if ( this.visible() === true ) {
-			this.marker.setMap( map );
-		} else {
-			this.marker.setMap( null );
-		}
-		return true;
-	}, this );
+	// this.showMarker = ko.computed( function () {
+	// 	if ( this.visible() === true ) {
+	// 		this.marker.setMap( map );
+	// 	} else {
+	// 		this.marker.setMap( null );
+	// 	}
+	// 	return true;
+	// }, this );
+	//
+	// this.marker.addListener( 'mouseover', function () {
+	// 	this.setIcon( highlightedIcon );
+	// } );
+	//
+	// this.marker.addListener( 'mouseout', function () {
+	// 	this.setIcon( defaultIcon );
+	// } );
 
-	this.marker.addListener( 'mouseover', function () {
-		this.setIcon( highlightedIcon );
-	} );
-
-	this.marker.addListener( 'mouseout', function () {
-		this.setIcon( defaultIcon );
-	} );
-
-	this.bounce = function ( place ) {
-		google.maps.event.trigger( self.marker, 'click' );
-	};
-
-	this.marker.addListener( 'click', function () {
-
-		self.infoWindow.setContent( self.info );
-
-		self.infoWindow.open( map, this );
-
-		self.marker.setAnimation( google.maps.Animation.BOUNCE );
-		setTimeout( function () {
-			self.marker.setAnimation( null );
-		}, 2100 );
-	} );
+	// this.bounce = function ( place ) {
+	// 	google.maps.event.trigger( self.marker, 'click' );
+	// };
+	//
+	// this.marker.addListener( 'click', function () {
+	//
+	// 	self.infoWindow.setContent( self.info );
+	//
+	// 	self.infoWindow.open( map, this );
+	//
+	// 	self.marker.setAnimation( google.maps.Animation.BOUNCE );
+	//
+	// 	setTimeout( function () {
+	// 		self.marker.setAnimation( null );
+	// 	}, 100 );
+	// } );
 
 	// map.addListener( 'center_changed', function () {
 	// 	// 3 seconds after the center of the map has changed, pan back to the
@@ -278,18 +322,18 @@ var Location = function ( data ) {
 	// 	}, 3000 );
 	// } );
 
-	this.marker.addListener( 'dblclick', function () {
-		self.map = self.marker.getMap();
-		//alert( self.map.getZoom() );
-		if ( self.map.getZoom() > 5 ) {
-			self.map.setCenter( 0, 0 );
-			self.map.setZoom( 3 );
-		} else {
-			self.map.setCenter( self.marker.getPosition() );
-			self.map.setZoom( 17 );
-			//self.map.panTo( self.marker.getPosition() );
-		}
-	} );
+	// this.marker.addListener( 'dblclick', function () {
+	// 	self.map = self.marker.getMap();
+	// 	//alert( self.map.getZoom() );
+	// 	if ( self.map.getZoom() > 5 ) {
+	// 		self.map.setCenter( 0, 0 );
+	// 		self.map.setZoom( 3 );
+	// 	} else {
+	// 		self.map.setCenter( self.marker.getPosition() );
+	// 		self.map.setZoom( 17 );
+	// 		//self.map.panTo( self.marker.getPosition() );
+	// 	}
+	// } );
 
 };
 
@@ -321,5 +365,7 @@ function ViewModel() {
 		}
 	}, self );
 
-
+	this.filteredList().forEach( function ( locationItem ) {
+		bounds.extend( filteredList.marker.getPosition() );
+	} );
 }
